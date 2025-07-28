8693d861c77de03bfe09502ec266a39ba9a42f1b
ganapathi9191 <ganapathivaraprasad123@gmail.com>
2025-07-28 20:49:52 +0530

    first4

--- routes/foodSystemRoute.js
+++ routes/foodSystemRoute.js
@@ -2,13 +2,16 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/foodSystemController");
 const upload = require("../utils/uploadMiddleware");
-const { protect, admin } = require('../utils/authMiddleware');
 
 router.post("/category", upload.single("image"), controller.createCategory);
 router.get("/category", controller.getAllCategories);
 
-router.post("/veg-food", upload.single("image"), controller.createVegFood); // ✅ Fixed
-router.get("/veg-food", controller.getAllVegFoods);                         // ✅ Fixed
+
+router.post('/veg-food', upload.single('image'), controller.createVegFood);
+router.get('/veg-foods', controller.getAllVegFoods);
+router.get('/veg-food/:id', controller.getVegFoodById);
+router.put('/veg-food/:id', upload.single('image'), controller.updateVegFood);
+router.delete('/veg-food/:id', controller.deleteVegFood);
 
 router.post("/cart", controller.addToCart);
 router.get("/cart/:userId", controller.getCart);
@@ -22,10 +25,10 @@ router.delete("/cart/:userId/:productId", controller.removeFromCart);
 router.post('/restaurant', upload.single('image'),controller.createRestaurant);
 
 // GET /api/restaurants - Get all restaurants
-router.get('/restaurant', controller.getRestaurants);
+router.get('/restaurant', controller.getAllRestaurants);
 
 // GET /api/restaurants/:id - Get single restaurant
-router.get('/restaurant/:id', controller.getRestaurant);
+router.get('/restaurant/:id', controller.getRestaurantById);
 
 // PUT /api/restaurants/:id - Update restaurant (Admin only)
 router.put('/restaurant/:id',upload.single('image'), controller.updateRestaurant);
@@ -33,7 +36,7 @@ router.put('/restaurant/:id',upload.single('image'), controller.updateRestaurant
 // DELETE /api/restaurants/:id - Delete restaurant (Admin only)
 router.delete('/restaurant/:id',controller.deleteRestaurant);
 
-router.get('/top', controller.getTopRatedRestaurants); // ✅ Top-rated route
+router.get('/top-nearby/:userId', controller.getTopRatedNearbyRestaurants); // ✅ Top-rated route
 
 // GET /api/restaurants/nearby/:userId - Get nearby restaurants
 router.get('/nearby/:userId', controller.getNearbyRestaurants);
